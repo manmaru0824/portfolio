d78ff011e370c8edfce1dce4b702c7e86d1e6033
manmaru0824 <165455265+manmaru0824@users.noreply.github.com>
2024-10-18 09:06:27 +0900

    タブメニューのサムネイル実装

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
 const worksSwiper = new Swiper(".works_images_swiper", {
-	loop: true, // ループする
-	// effect: "cube", // キューブのエフェクト
-	spaceBetween: 20, // スライド間の余白
-	slidesPerView: 3.5, // 画面に表示するスライド数
-	centeredSlides: true, // アクティブなスライドを中央に表示
-	speed: 6000, // 少しゆっくり(デフォルトは300)
-	autoplay: {
-		// 自動再生
-		delay: 0, // 0秒後に次のスライド
-	},
+  loop: true, // ループする
+  // effect: "cube", // キューブのエフェクト
+  spaceBetween: 20, // スライド間の余白
+  slidesPerView: 3.5, // 画面に表示するスライド数
+  centeredSlides: true, // アクティブなスライドを中央に表示
+  speed: 6000, // 少しゆっくり(デフォルトは300)
+  autoplay: {
+    // 自動再生
+    delay: 0, // 0秒後に次のスライド
+  },
 });
-
-const booksSwiper = new Swiper(".works_books_swiper", {
-	loop: true,
-	// effect: "coverflow",
-	spaceBetween: 20,
-	slidesPerView: 3.5,
-	centeredSlides: true,
-	autoPlay: true,
-	speed: 6000,
-	autoplay: {
-		delay: 0,
-		reverseDirection: true, // 逆方向にスライド
-	},
+// サムネイル
+const sliderThumbnail = new Swiper(".slider-thumbnail", {
+  slidesPerView: 3, // サムネイルの枚数
 });
-
-const cardsSwiper = new Swiper(".works_cards_swiper", {
-	loop: true,
-	// effect: "flip",
-	spaceBetween: 20,
-	slidesPerView: 3.5,
-	centeredSlides: true,
-	autoPlay: true,
-	speed: 1000,
-	autoplay: {
-		delay: 1500,
-	},
+// スライダー
+const slider = new Swiper(".slider", {
+  loop: true,
+  effect: "fade", // フェード
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
 });
